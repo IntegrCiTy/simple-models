3fc7b20e76d4ddcc1eb7855bcd487e151f392630
Pablo Puerto <pablo.puerto@crem.ch>
2018-06-15 10:19:06 +0200

    hp error

--- energytechnomodels/hp.py
+++ energytechnomodels/hp.py
@@ -28,7 +28,7 @@ class HeatPump(Model):
         self.fluid_snk = Chemical(fluid_snk, T=273.15 + t_snk, P=3E5)
 
         self.p_max = p_max
-        self.t_src = t_src
+        self.t_i_src = t_src
         self.t_snk = t_snk
 
         self.t_i_snk = t_snk - 10.0
@@ -41,19 +41,19 @@ class HeatPump(Model):
 
         self.io = io_init
 
-        self.cop = self.n_th * ((self.t_snk + 273.15) / (self.t_snk - self.t_src))
+        self.cop = self.n_th * ((self.t_snk + 273.15) / (self.t_snk - self.t_i_src))
 
         self.p_sink = self.io * self.p_max
         self.p_elec = self.p_sink / self.cop
         self.p_srce = self.p_sink - self.p_elec
 
         self.t_o_snk = self.t_i_snk + self.p_sink / self.m_dot_snk / self.fluid_snk.Cp
-        self.t_o_src = self.t_src - self.p_srce / self.m_dot_src / self.fluid_src.Cp
+        self.t_o_src = self.t_i_src - self.p_srce / self.m_dot_src / self.fluid_src.Cp
 
     def step(self, step, unit="seconds"):
         super().step(step, unit)
 
-        self.fluid_src.T = (self.t_src + self.t_o_src) / 2 + 273.15
+        self.fluid_src.T = (self.t_i_src + self.t_o_src) / 2 + 273.15
         self.fluid_snk.T = (self.t_i_snk + self.t_o_snk) / 2 + 273.15
 
         t = np.arange(start=0, stop=step * self.UNIT[unit], step=1.0)
@@ -62,11 +62,11 @@ class HeatPump(Model):
             tau_model, self.p_sink, t, args=(self.io, self.tau, self.p_max)
         )
 
-        self.cop = self.n_th * ((self.t_snk + 273.15) / (self.t_snk - self.t_src))
+        self.cop = self.n_th * ((self.t_snk + 273.15) / (self.t_snk - self.t_i_src))
 
         self.p_sink = round(res_p_sink[-1][0])
         self.p_elec = self.p_sink / self.cop
         self.p_srce = self.p_sink - self.p_elec
 
         self.t_o_snk = self.t_i_snk + self.p_sink / self.m_dot_snk / self.fluid_snk.Cp
-        self.t_o_src = self.t_src - self.p_srce / self.m_dot_src / self.fluid_src.Cp
+        self.t_o_src = self.t_i_src - self.p_srce / self.m_dot_src / self.fluid_src.Cp
